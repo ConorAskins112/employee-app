b4b378c2eae530e8894567f5fa43704b617718c5
ConorAskins112 <20079431@mail.wit.ie>
2023-01-27 23:42:16 +0000

    Employee App 1.0 with Rounding added

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,57 @@
+package ie.setu.main.kt
+
+val firstName = "Joe"
+val surname = "Soap"
+val gender='m'
+val employeeId=6143
+val grossSalary= 67543.21
+val payePercentage = 38.5
+val prsiPercentage = 5.2
+val annualBonus = 1450.50
+val cycleToWorkScheme = 54.33
+//payslip calculations
+var monthlySalary = grossSalary/12
+var monthPaye = monthlySalary*(payePercentage/100)
+var monthlyPrsi = monthlySalary*(prsiPercentage/100)
+var totalDeductions = monthPaye+monthlyPrsi+cycleToWorkScheme
+var bonus = annualBonus/12
+var gross= monthlySalary+ bonus
+var netPay = gross-totalDeductions
+
+//+ firstName.uppercase()+" "+ surname.uppercase()+"("+ gender.uppercase()+")"+
+fun main(args: Array<String>) {
+
+    println("Pay slip Printer")
+   payslipLayout()
+
+
+}
+
+
+
+fun payslipLayout(){
+    println("____________________________________________________________")
+    println("|                   Monthly Payslip                        |")
+    println("|                                                          |")
+    println("|__________________________________________________________|")
+    println("|                                                          |")
+    println("| Employee Name: "+firstName.uppercase()+" "+ surname.uppercase()+"("+ gender.uppercase()+")"+"            Employee ID:$employeeId   |")
+    println("|                                                          |")
+    println("|__________________________________________________________|")
+    println("|                                                          |")
+    println("|    PAYMENT DETAILS          DEDUCTION DETAILS            |")
+    println("|__________________________________________________________|")
+    println("|                                                          |")
+    println("|    Salary:"+ rounding(monthlySalary)+"           PAYE:"+ rounding(monthPaye)+"                 |")
+    println("|    bonus:"+ rounding(bonus)+"             PRSI:"+ rounding(monthlyPrsi)+"                  |")
+    println("|                             Cycle To Work:$cycleToWorkScheme          |")
+    println("|__________________________________________________________|")
+    println("|    Gross:"+ rounding(gross)+"            Total Deductions:"+ rounding(totalDeductions)+"     |")
+    println("|__________________________________________________________|")
+    println("|                    NET PAY:"+ rounding(netPay)+"                       |")
+    println("|__________________________________________________________|")
+}
+
+fun rounding(num:Double): String {
+    return "%.2f".format(num)
+}
